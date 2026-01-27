eaf67441f9c538829e4a90c485343efb5e317235
rubenloading <ruben.ludwig@icloud.com>
2026-01-27 11:53:17 +0100

    added remove tasks

--- DayPlanner.cs
+++ DayPlanner.cs
@@ -16,10 +16,11 @@ namespace DayPlanner
             Console.WriteLine("What do you wanna do ?");
             Console.WriteLine("Press 1 for seeing your tasks for today");
             Console.WriteLine("Press 2 for adding Tasks for a specific date");
+            Console.WriteLine("Press 3 to remove something from the list");
             string input = Console.ReadLine();
             Int32.TryParse(input, out decision); 
                 Console.WriteLine(decision);
-                if(decision != 1 && decision != 2)
+                if(decision != 1 && decision != 2 && decision != 3)
                 {
                     Console.WriteLine("Try again");
                 }
@@ -37,8 +38,44 @@ namespace DayPlanner
                 {
                     Console.WriteLine("Gib die Aufgabe ein: ");
                     string task = Console.ReadLine();
-                    File.AppendAllText(filePath, task + "/n");
+                    File.AppendAllText(filePath, task + Environment.NewLine);
                     Console.WriteLine("Aufgabe gespeichert!");
+                }
+                else if (decision == 3)
+                {
+                    if(File.Exists(filePath))
+                    {
+                        string [] tasks = File.ReadAllLines(filePath);
+
+                        if (tasks.Length == 0)
+                             {
+                                 Console.WriteLine("Keine Aufgabe zum Löschen!");
+                             }
+                            else
+                            {
+                                Console.WriteLine("Deine Aufgaben: ");
+                                for(int i = 0; i < tasks.Length; i++)
+                                {
+                                    Console.WriteLine($"{i + 1}. {tasks[i]}");
+                                }
+
+                                Console.WriteLine("Welche Aufgabe willst du löschen? Gib die Nummer ein: ");
+                                string input2 = Console.ReadLine();
+                                if (int.TryParse(input2, out int taskNumber) && taskNumber >= 1 && taskNumber <= tasks.Length)
+                                {
+                                    var taskList = tasks.ToList();
+                                    taskList.RemoveAt(taskNumber -1);
+
+                                    File.WriteAllLines(filePath, taskList);
+                                    Console.WriteLine("Aufgabe Geöscht!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Keine Aufgabe gefunden!");
+                                }
+                            }
+                    }
+
                 }
 
         }
