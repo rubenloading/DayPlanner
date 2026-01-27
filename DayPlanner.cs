a4160c50a553474de1fb4f3dfab0eefb6f55186b
rubenloading <ruben.ludwig@icloud.com>
2026-01-27 08:44:11 +0100

    Created Project DayPlanner.cs

---
+++ DayPlanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DayPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
