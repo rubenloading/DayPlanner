699bd986b9b62a7476d02c4460b913888aa39d37
rubenloading <ruben.ludwig@icloud.com>
2026-01-27 10:24:18 +0100

    Added the base Project, still need to work on out and date specific input

--- DayPlanner.cs
+++ DayPlanner.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text;
+using System.IO;
+using System.Threading.Tasks; 
 
 namespace DayPlanner
 {
@@ -6,7 +11,36 @@ namespace DayPlanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filePath = "tasks.txt"; 
+            int decision = 0; 
+            Console.WriteLine("What do you wanna do ?");
+            Console.WriteLine("Press 1 for seeing your tasks for today");
+            Console.WriteLine("Press 2 for adding Tasks for a specific date");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out decision); 
+                Console.WriteLine(decision);
+                if(decision != 1 && decision != 2)
+                {
+                    Console.WriteLine("Try again");
+                }
+                if (decision == 1)
+                {
+                    if(File.Exists(filePath))
+                    {
+                    string tasks = File.ReadAllText(filePath);
+                    Console.WriteLine("Deine Aufgaben: ");
+                    Console.WriteLine(tasks);
+                    }
+                    else {Console.WriteLine("Keine Aufgaben gefunden");}
+                }
+                else if (decision == 2)
+                {
+                    Console.WriteLine("Gib die Aufgabe ein: ");
+                    string task = Console.ReadLine();
+                    File.AppendAllText(filePath, task + "/n");
+                    Console.WriteLine("Aufgabe gespeichert!");
+                }
+
         }
     }
 }
--- obj/Debug/net8.0/DayPlanner.AssemblyInfo.cs
+++ obj/Debug/net8.0/DayPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a844525303d8643e955a7062112f813effc159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4160c50a553474de1fb4f3dfab0eefb6f55186b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
