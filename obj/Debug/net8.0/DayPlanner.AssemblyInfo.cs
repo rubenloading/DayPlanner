e52feb6ecb6d65f5cbdeb2b250d9cd3bae4272ff
rubenloading <ruben.ludwig@icloud.com>
2026-03-20 15:26:23 +0100

    fixed build errors only existing on windows

--- obj/Debug/net8.0/DayPlanner.AssemblyInfo.cs
+++ obj/Debug/net8.0/DayPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8c3d409d8304f50979c867ed3a8529a2b5bfc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6556cf5391cb43bd6f6547f6f84e31927f78bf27")]
 [assembly: System.Reflection.AssemblyProductAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DayPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
